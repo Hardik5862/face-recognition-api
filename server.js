b80d8d897b42966f28c06aea7066fe916e70cbd5
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-08-02 22:50:10 +0530

    fixing errors

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'postgres',
-    password : 'localhost',
+    user : 'hardik',
+    password : '',
     database : 'facedetectapp'
   }
 });
