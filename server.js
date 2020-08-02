1fd1f466080b38459642ef7e7ca06cc48bd0c97a
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-08-02 22:15:43 +0530

    fixing error

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => { res.send('it is working')})
 app.post('/signin', (req, res) => {signin.signinHandler(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.registerHandler(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
