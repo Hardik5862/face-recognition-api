1176aac33a723c8daa0026dc66d9c04fd6a0d698
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-08-03 21:51:30 +0530

    fix

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('it is working')});
+app.get('/', (req, res) => {res.send('it is working')})
 app.post('/signin', (req, res) => {signin.signinHandler(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.registerHandler(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('App is running');
 })
